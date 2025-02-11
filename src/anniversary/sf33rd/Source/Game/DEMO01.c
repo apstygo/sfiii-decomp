fadd39d3dfbdd26d738577680434aec7e3665fb5
Artem Pstygo <apstygo.dev@gmail.com>
2025-02-11 23:40:33 +0300

    Init DEMO01.c

---
+++ src/anniversary/sf33rd/Source/Game/DEMO01.c
@@ -0,0 +1,5 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/DEMO01", Title);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/DEMO01", Title_At_a_Dash);
