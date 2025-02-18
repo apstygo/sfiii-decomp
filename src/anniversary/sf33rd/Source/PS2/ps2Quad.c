20360997d37cdaa447f2006399a145d9301cbe0b
Artem Pstygo <apstygo.dev@gmail.com>
2025-02-19 01:09:52 +0300

    Decompile ps2SeqsRenderQuad_A2

--- src/anniversary/sf33rd/Source/PS2/ps2Quad.c
+++ src/anniversary/sf33rd/Source/PS2/ps2Quad.c
@@ -15,6 +15,13 @@ typedef struct {
 } _TexCoord;
 
 typedef struct {
+    // total size: 0x54
+    Point v[4];     // offset 0x0, size 0x30
+    _TexCoord t[4]; // offset 0x30, size 0x20
+    u32 texCode;    // offset 0x50, size 0x4
+} _Sprite;
+
+typedef struct {
     // total size: 0x34
     Point v[2];     // offset 0x0, size 0x18
     _TexCoord t[2]; // offset 0x18, size 0x10
@@ -51,9 +58,8 @@ void ps2SeqsRenderQuad_Ax(Sprite2 *spr) {
     *((u32 *)p)++ = SCE_VIF1_SET_NOP(0);
     *((u32 *)p)++ = SCE_VIF1_SET_DIRECTHL(4, 0);
 
-    *p++ = SCE_GIF_SET_TAG(1, SCE_GS_TRUE, SCE_GS_FALSE, 0, SCE_GIF_REGLIST, 6);
-    *p++ = 0 | SCE_GS_PRIM << (4 * 0) | SCE_GS_RGBAQ << (4 * 1) | SCE_GS_ST << (4 * 2) | SCE_GS_XYZ2 << (4 * 3) |
-           SCE_GS_ST << (4 * 4) | SCE_GS_XYZ2 << (4 * 5);
+    *p++ = SCE_GIF_SET_TAG(1, 1, 0, 0, SCE_GIF_REGLIST, 6);
+    *p++ = SCE_GS_PRIM | SCE_GS_RGBAQ << 4 | SCE_GS_ST << 8 | SCE_GS_XYZ2 << 12 | SCE_GS_ST << 16 | SCE_GS_XYZ2 << 20;
 
     col = flPS2ConvColor(spr->vtxColor, 0);
     cA = (col >> 24) & 0xFF;
@@ -64,14 +70,14 @@ void ps2SeqsRenderQuad_Ax(Sprite2 *spr) {
     z = flPS2ConvScreenFZ(spr->v[0].z);
 
     *p++ = SCE_GS_SET_PRIM(SCE_GS_PRIM_SPRITE,
-                           /* Gourand */ SCE_GS_FALSE,
-                           /* Textured */ SCE_GS_TRUE,
-                           /* fog */ SCE_GS_FALSE,
-                           /* Alpha blend */ SCE_GS_TRUE,
-                           /* AA */ SCE_GS_FALSE,
-                           /* UV */ SCE_GS_FALSE,
-                           /* ctx2 */ SCE_GS_FALSE,
-                           /* frag */ SCE_GS_FALSE);
+                           /* Gourand */ 0,
+                           /* Textured */ 1,
+                           /* fog */ 0,
+                           /* Alpha blend */ 1,
+                           /* AA */ 0,
+                           /* UV */ 0,
+                           /* ctx2 */ 0,
+                           /* frag */ 0);
     *p++ = rgbaq;
 
     *p++ = SCE_GS_SET_ST(((_Sprite2 *)spr)->t[0].s, ((_Sprite2 *)spr)->t[0].t);
@@ -89,7 +95,62 @@ void ps2SeqsRenderQuad_Ax(Sprite2 *spr) {
     flPS2DmaAddQueue2(0, DMArefs | (data_ptr & 0xFFFFFFF), data_ptr, &flPs2VIF1Control);
 }
 
-INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/PS2/ps2Quad", ps2SeqsRenderQuad_A2);
+void ps2SeqsRenderQuad_A2(Sprite *spr, u32 col) {
+    u32 data_ptr;
+    u64 rgbaq;
+    u64 *p;
+    s32 x;
+    s32 y;
+    s32 z;
+    u8 cA;
+    u8 cR;
+    u8 cG;
+    u8 cB;
+
+    data_ptr = flPS2GetSystemTmpBuff(0x50, 0x10);
+    p = (u64 *)data_ptr;
+
+    *p++ = 0x80000000 | DMAend | 4;
+    *((u32 *)p)++ = SCE_VIF1_SET_NOP(0);
+    *((u32 *)p)++ = SCE_VIF1_SET_DIRECTHL(4, 0);
+
+    *p++ = SCE_GIF_SET_TAG(1, SCE_GS_TRUE, SCE_GS_FALSE, 0, SCE_GIF_REGLIST, 6);
+    *p++ = SCE_GS_PRIM | SCE_GS_RGBAQ << 4 | SCE_GS_ST << 8 | SCE_GS_XYZ2 << 12 | SCE_GS_ST << 16 | SCE_GS_XYZ2 << 20;
+
+    col = flPS2ConvColor(col, 0);
+    cA = (col >> 24) & 0xFF;
+    cR = (col >> 16) & 0xFF;
+    cG = (col >> 8) & 0xFF;
+    cB = col & 0xFF;
+    rgbaq = SCE_GS_SET_RGBAQ(cR, cG, cB, cA, 0x3F800000);
+    z = flPS2ConvScreenFZ(spr->v[0].z);
+
+    *p++ = SCE_GS_SET_PRIM(SCE_GS_PRIM_SPRITE,
+                           /* Gourand */ 0,
+                           /* Textured */ 1,
+                           /* fog */ 0,
+                           /* Alpha blend */ 1,
+                           /* AA */ 0,
+                           /* UV */ 0,
+                           /* ctx2 */ 0,
+                           /* frag */ 0);
+
+    *p++ = rgbaq;
+
+    *p++ = SCE_GS_SET_ST(((_Sprite *)spr)->t[0].s, ((_Sprite *)spr)->t[0].t);
+
+    x = spr->v[0].x + 0.5f;
+    y = spr->v[0].y + 0.5f;
+    *p++ = SCE_GS_SET_XYZ((flPs2State.D2dOffsetX + x) << 4, (flPs2State.D2dOffsetY + y) << 4, (u32)z);
+
+    *p++ = SCE_GS_SET_ST(((_Sprite *)spr)->t[3].s, ((_Sprite *)spr)->t[3].t);
+
+    x = spr->v[3].x + 0.5f;
+    y = spr->v[3].y + 0.5f;
+    *p++ = SCE_GS_SET_XYZ((flPs2State.D2dOffsetX + x) << 4, (flPs2State.D2dOffsetY + y) << 4, (u32)z);
+
+    flPS2DmaAddQueue2(0, DMArefs | (data_ptr & 0xFFFFFFF), data_ptr, &flPs2VIF1Control);
+}
 
 INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/PS2/ps2Quad", ps2SeqsRenderQuad_A);
 
