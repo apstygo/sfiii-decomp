dd6cf88805f2d58460b18305dccee70172e2ebfd
Artem Pstygo <apstygo.dev@gmail.com>
2025-02-22 11:49:17 +0300

    Decompile flmwFlipSub

--- src/anniversary/sf33rd/AcrSDK/MiddleWare/PS2/acrmw.c
+++ src/anniversary/sf33rd/AcrSDK/MiddleWare/PS2/acrmw.c
@@ -1,11 +1,19 @@
 #include "sf33rd/AcrSDK/MiddleWare/PS2/acrmw.h"
 #include "common.h"
 #include "sf33rd/AcrSDK/MiddleWare/PS2/ADX/flADX.h"
+#include "sf33rd/AcrSDK/ps2/foundaps2.h"
 
 void flmwVSyncCallback() {
     flAdxControll(0);
 }
 
-INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/AcrSDK/MiddleWare/PS2/acrmw", flmwFlipSub);
+static void flmwFlipSub() {
+    if (flPS2FlipCancelFlag > 0) {
+        flPS2FlipCancelFlag -= 1;
+        return;
+    }
+
+    flPs2State.Db_change_enable = 1;
+}
 
 INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/AcrSDK/MiddleWare/PS2/acrmw", flmwFlip);
