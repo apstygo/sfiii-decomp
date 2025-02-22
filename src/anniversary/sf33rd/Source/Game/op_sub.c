0e08d77dd12b7e9a3d2fcbcb6c343296128b7c7d
Artem Pstygo <apstygo.dev@gmail.com>
2025-02-22 14:20:51 +0300

    Init op_sub.c

---
+++ src/anniversary/sf33rd/Source/Game/op_sub.c
@@ -0,0 +1,11 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/op_sub", TexRelease);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/op_sub", TexRelease_OP);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/op_sub", put_chr2);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/op_sub", opbg_trans);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/op_sub", COLOR_COPYn);
