b39cac8e02a8ec9b109e536f2a62e6860f6d1696
Artem Pstygo <apstygo.dev@gmail.com>
2025-02-22 12:06:36 +0300

    Initialize MemMan.c

---
+++ src/anniversary/sf33rd/Source/Common/MemMan.c
@@ -0,0 +1,21 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Common/MemMan", mmSystemInitialize);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Common/MemMan", mmHeapInitialize);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Common/MemMan", mmRoundUp);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Common/MemMan", mmRoundOff);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Common/MemMan", mmDebWriteTag);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Common/MemMan", mmGetRemainder);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Common/MemMan", mmGetRemainderMin);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Common/MemMan", mmAlloc);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Common/MemMan", mmAllocSub);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Common/MemMan", mmFree);
