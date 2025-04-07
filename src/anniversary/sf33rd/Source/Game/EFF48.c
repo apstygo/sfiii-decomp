7c6566242e9e0081d1ac31ed2b7880f76886b7ba
Artem Pstygo <apstygo.dev@gmail.com>
2025-04-07 21:12:31 +0300

    Init EFF48.c

---
+++ src/anniversary/sf33rd/Source/Game/EFF48.c
@@ -0,0 +1,55 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFF48", effect_48_move);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFF48", eff48_0000);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFF48", eff48_1000);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFF48", effect_48_init);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFF48", eff48_num_tbl);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFF48", eff48_data_tbl00);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFF48", eff48_data_tbl01);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFF48", eff48_data_tbl02);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFF48", eff48_data_tbl03);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFF48", eff48_data_tbl04);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFF48", eff48_data_tbl05);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFF48", eff48_data_tbl06);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFF48", eff48_data_tbl07);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFF48", eff48_data_tbl08);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFF48", eff48_data_tbl09);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFF48", eff48_data_tbl10);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFF48", eff48_data_tbl11);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFF48", eff48_data_tbl12);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFF48", eff48_data_tbl13);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFF48", eff48_data_tbl14);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFF48", eff48_data_tbl15);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFF48", eff48_data_tbl16);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFF48", eff48_data_tbl17);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFF48", eff48_data_tbl18);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFF48", eff48_data_tbl19);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFF48", eff48_data_tbl20);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFF48", eff48_data_tbl21);
