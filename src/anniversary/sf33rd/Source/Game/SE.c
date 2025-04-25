df3098e761acb640cc2aac887f3aff890ddc7605
Artem Pstygo <apstygo.dev@gmail.com>
2025-04-25 18:03:37 +0300

    Decompile Disp_Sound_Code

--- src/anniversary/sf33rd/Source/Game/SE.c
+++ src/anniversary/sf33rd/Source/Game/SE.c
@@ -1,4 +1,12 @@
+#include "sf33rd/Source/Game/SE.h"
 #include "common.h"
+#include "sf33rd/AcrSDK/ps2/flps2debug.h"
+#include "sf33rd/Source/Game/debug/Debug.h"
+#include "structs.h"
+
+#define SDEB_SIZE 8
+
+extern SoundPatch sdeb[SDEB_SIZE]; // size: 0x60, address: 0x5797F0
 
 INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/SE", BGM_Stage_Data);
 INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/SE", SE_Shock_Data);
@@ -42,7 +50,23 @@ INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/SE", Check_Bonus_SE
 
 INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/SE", Store_Sound_Code);
 
-INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/SE", literal_427_0054F8B0);
-INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/SE", literal_428_0054F8B8);
-INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/SE", literal_429_0054F8C0);
-INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/SE", Disp_Sound_Code);
+void Disp_Sound_Code() {
+    s16 i;
+
+    if (Debug_w[4] == 0) {
+        return;
+    }
+
+    flPrintColor(0xFFFFFFFF);
+
+    for (i = 0; i < SDEB_SIZE; i++) {
+        flPrintL(2, i * 2 + 11, "%3X", sdeb[i].cp3code);
+        flPrintL(50, i * 2 + 11, "%2X", sdeb[i].rmc.ptix);
+        flPrintL(53, i * 2 + 11, "%2X", sdeb[i].rmc.bank);
+        flPrintL(56, i * 2 + 11, "%2X", sdeb[i].rmc.port);
+        flPrintL(59, i * 2 + 11, "%3X", sdeb[i].rmc.code);
+    }
+
+    flPrintColor(0xFFFFFF8F);
+    flPrintL(2, 9, "CODE  KDCM PORT BKCD PRIO VOL  POS  PICH NEXT   PX BK PO CODE");
+}
