ff24b5e1ff523eb3ccc3505035211364b10f713d
Artem Pstygo <apstygo.dev@gmail.com>
2025-04-28 19:19:27 +0300

    Initialize ta_sub.c

---
+++ src/anniversary/sf33rd/Source/Game/ta_sub.c
@@ -0,0 +1,56 @@
+#include "sf33rd/Source/Game/ta_sub.h"
+#include "common.h"
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/ta_sub", sync_fam_set3);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/ta_sub", range_x_check);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/ta_sub", range_x_check3);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/ta_sub", range_y_check);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/ta_sub", add_x_sub);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/ta_sub", add_x_sub2);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/ta_sub", add_y_sub);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/ta_sub", add_y_sub2);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/ta_sub", obr_no_disp_check);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/ta_sub", disp_pos_trans_entry);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/ta_sub", disp_pos_trans_entry5);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/ta_sub", disp_pos_trans_entry_r);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/ta_sub", disp_pos_trans_entry_r4);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/ta_sub", disp_pos_trans_entry_s);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/ta_sub", disp_pos_trans_entry_rs);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/ta_sub", pl_eff_trans_entry);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/ta_sub", eff_hit_check);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/ta_sub", eff_hit_check_sub);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/ta_sub", eff_hit_check2);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/ta_sub", eff_hit_check_sub2);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/ta_sub", hit_check_subroutine_yu);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/ta_sub", eff_hit_flag_clear);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/ta_sub", compel_dead_check);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/ta_sub", win_lose_work_clear);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/ta_sub", cal_bg_speed_data_x);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/ta_sub", cal_bg_speed_data_y);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/ta_sub", pl_hit_eff);
