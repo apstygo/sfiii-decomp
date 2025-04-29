4263969953929dd8d199ff7c232b19f4403b1938
Artem Pstygo <apstygo.dev@gmail.com>
2025-04-29 20:07:44 +0300

    Init adx_inis

---
+++ src/anniversary/cri/libadxe/adx_inis.c
@@ -0,0 +1,25 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/adx_inis", ADXT_ConfigVsyncSvr);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/adx_inis", adxini_rnaerr_cbfn);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/adx_inis", adxini_lscerr_cbfn);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/adx_inis", ADXT_VsyncProc);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/adx_inis", adxt_exec_main_thrd);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/adx_inis", adxt_exec_main_nothrd);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/adx_inis", adxt_exec_tsvr);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/adx_inis", adxt_exec_fssvr);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/adx_inis", ADXT_Init);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/adx_inis", ADXT_ResetLibrary);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/adx_inis", ADXT_Finish);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/adx_inis", D_0055A8C8);
