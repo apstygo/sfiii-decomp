a5f634ecef429787b80e4b4bc65aa20731626995
Artem Pstygo <apstygo.dev@gmail.com>
2025-04-29 15:37:28 +0300

    Initialize svm.c

---
+++ src/anniversary/cri/libadxe/svm.c
@@ -0,0 +1,91 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", _svm_lock);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", _svm_unlock);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_Lock);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_Unlock);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_LockVar);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_LockSync);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_LockRsc);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_LockThrd);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_LockEtc);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_UnlockVar);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_UnlockSync);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_UnlockRsc);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_UnlockThrd);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_UnlockEtc);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_GetLockType);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_CallErr);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_CallErr1);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_CallErr2);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_ItoA);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_ItoA2);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_SetCbSvr);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_DelCbSvr);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_SetCbSvrId);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_SetCbBdr);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_GotoSvrBorder);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_SetCbErr);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_SetCbLock);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_SetCbUnlock);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_ExecSvrFunc);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_ExecSvrFuncId);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_ExecSvrVint);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_ExecSvrUsrVsync);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_ExecSvrVsync);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_ExecSvrUhigh);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_ExecSvrFs);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_ExecSvrMain);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_ExecSvrMwIdle);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_ExecSvrUsrIdle);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_GetExecCount);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", svm_reset_variable);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_Init);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_Finish);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_TestAndSet);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_SetCbTestAndSet);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/svm", SVM_DelCbTestAndSet);
