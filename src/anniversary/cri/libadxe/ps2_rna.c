ac06b05ebf3a86c7fd311ca1c9b6d6a176b23d30
Artem Pstygo <apstygo.dev@gmail.com>
2025-05-03 14:07:37 +0300

    Init ps2_rna

---
+++ src/anniversary/cri/libadxe/ps2_rna.c
@@ -0,0 +1,137 @@
+#include "common.h"
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", D_0055E0C8);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", D_0055E0F8);
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", D_0055E128);
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", D_0055E158);
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_SetupVoice);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", D_0055E198);
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", D_0055E1E0); // used in PS2RNA_Init
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", D_0055E210);
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", D_0055E240);
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", D_0055E278);
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", D_0055E2A8);
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", D_0055E2E0);
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", ps2rna_init_psj);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", ps2rna_finish_psj);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", ps2rna_get_psj);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", ps2rna_release_psj);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", ps2rna_rcvcbf);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", ps2rna_sndcbf);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", D_0055E318);
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", D_0055E360);
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", D_0055E388);
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_Init);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_RestoreIopSnd);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_InitIopSnd);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_FinishIopSnd);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_SetupDolby);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_ShutdownDolby);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_SetStmType);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_Finish);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", D_0055E3C8);
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", D_0055E3F0);
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", D_0055E418);
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_Create);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_Destroy);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", D_0055E440);
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_Start);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_Stop);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_SetTransSw);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_SetPlaySw);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_PauseAll);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_GetStatPauseAll);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_SetDiscardSw);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", D_0055E460);
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_ClearBuf);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", D_0055E488);
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_SetPcmType);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_GetTime);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_GetNumData);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_GetNumRoom);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_ExecHndl);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_ExecServer);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", D_0055E4B0);
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_SetStartSmpl);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_SetNumChan);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", D_0055E4D8);
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_SetSfreq);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_SetOutVol);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_SetOutPan);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_SetOutBalance);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", D_0055E4F8);
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_SetBitPerSmpl);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", D_0055E528);
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_GetStartSmpl);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_GetSfreq);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_GetOutVol);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_GetOutPan);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_GetOutBalance);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_GetBitPerSmpl);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", D_0055E550);
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_IsOverflow);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", D_0055E578);
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_ClearOverflow);
+
+void PS2RNA_Flush() {
+    // Do nothing
+}
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", D_0055E5A0);
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_DiscardData);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", D_0055E5C8);
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_SetTotalNumSmpl);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", D_0055E5F8);
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_SetStmHdInfo);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_GetSjtmp);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/ps2_rna", PS2RNA_GetSjiop);
