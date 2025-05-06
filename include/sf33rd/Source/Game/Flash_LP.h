48b36d93e28a19868c016a5499261a1dacbb175c
Xkower8181 <xkowerlolz@gmail.com>
2025-05-06 22:19:38 +0200

    Decompiled Flash_LP

--- include/sf33rd/Source/Game/Flash_LP.h
+++ include/sf33rd/Source/Game/Flash_LP.h
@@ -1,6 +1,11 @@
 #ifndef FLASH_LP_H
 #define FLASH_LP_H
 
+#include "structs.h"
+#include "types.h"
+
+extern u8 Lamp_Flash_Data[][2] = {{0x07, 0x6f}, {0x1e, 0x03}}; // size: 0x4, address: 0x51FC30 //Exclusive
+
 void Flash_Lamp();
 
 #endif
--- include/sf33rd/Source/Game/sc_data.h
+++ include/sf33rd/Source/Game/sc_data.h
@@ -3,6 +3,7 @@
 
 #include "types.h"
 
+extern u8 vmark_tbl[] = {0x13, 0x11, 0x0f, 0x0d, 0x1b, 0x1d, 0x1f, 0x21}; // size: 0x8, address: 0x54F140
 extern const s16 Fade_Pos_tbl[8];
 extern const s16 fade_data_tbl[][3];
 
