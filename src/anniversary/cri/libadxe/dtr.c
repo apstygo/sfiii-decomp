d762fa8e2eac895db06f0f8c1ec2823ac45c210e
Artem Pstygo <apstygo.dev@gmail.com>
2025-05-09 21:28:36 +0300

    Init dtr.c

---
+++ src/anniversary/cri/libadxe/dtr.c
@@ -0,0 +1,31 @@
+#include "common.h"
+#include <cri/private/libadxe/dtr.h>
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/dtr", D_0055CDD0);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/dtr", D_0055CE00);
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/dtr", D_0055CE20);
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/dtr", D_0055CE48);
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/dtr", D_0055CE78);
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/dtr", D_0055CEA0);
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/dtr", D_0055CEC8);
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/dtr", DTR_ExecHndl);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/dtr", DTR_GetStat);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/dtr", DTR_ExecServer);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/dtr", D_0055CEF0);
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/dtr", D_0055CF08);
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/dtr", DTR_Init);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/dtr", DTR_Finish);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/dtr", D_0055CF18);
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/dtr", DTR_Create);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/dtr", DTR_Destroy);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/dtr", DTR_Start);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/dtr", DTR_Stop);
