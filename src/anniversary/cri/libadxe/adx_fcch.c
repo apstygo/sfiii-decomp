c596053a623476d137e1da7342194efe4dd7d311
Xkower8181 <xkowerlolz@gmail.com>
2025-05-10 23:11:49 +0200

    Small tweaks to adx_fcch.c

--- src/anniversary/cri/libadxe/adx_fcch.c
+++ src/anniversary/cri/libadxe/adx_fcch.c
@@ -1,7 +1,7 @@
 #include "cri/private/libadxe/adx_fcch.h"
 #include "common.h"
-#include "sdk/eekernel.h"
+#include <eekernel.h>
 
 void ADXF_Ocbi(Sint8 *buf, Sint32 bsize) {
-    InvalidDCache(buf, &buf[bsize] - 1);
+    InvalidDCache(buf, buf + bsize - 1);
 }
