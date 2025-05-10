31efd4c5db138eb87860e1ef5f1f9b8667855965
Artem Pstygo <apstygo.dev@gmail.com>
2025-05-10 14:18:25 +0300

    Init dtx.c

---
+++ src/anniversary/cri/libadxe/dtx.c
@@ -0,0 +1,45 @@
+#include "common.h"
+#include <cri/private/libadxe/dtx.h>
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/dtx", D_0055CF30);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/dtx", dtx_rpc_func);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/dtx", dtx_svr_proc);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/dtx", dtx_create_rmt);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/dtx", dtx_destroy_rmt);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/dtx", dtx_def_rcvcbf);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/dtx", D_0055CF60);
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/dtx", dtx_def_sndcbf);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/dtx", DTX_Open);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/dtx", DTX_Close);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/dtx", D_0055CF78);
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/dtx", D_0055CF98);
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/dtx", D_0055CFB8);
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/dtx", D_0055CFD0);
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/dtx", D_0055CFE8);
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/dtx", DTX_Create);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/dtx", DTX_Destroy);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/dtx", DTX_SetRcvCbf);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/dtx", DTX_SetSndCbf);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/dtx", DTX_ExecHndl);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/dtx", D_0055D010);
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/dtx", DTX_Init);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/dtx", DTX_Finish);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/dtx", DTX_CallUrpc);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/dtx", DTX_ExecServer);
