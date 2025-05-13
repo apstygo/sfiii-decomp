8df84f714b7e7093d2ea8cf9b6042ddc8bd9710e
Artem Pstygo <apstygo.dev@gmail.com>
2025-05-13 16:36:19 +0300

    Init sj_uni.c

---
+++ src/anniversary/cri/libadxe/sj_uni.c
@@ -0,0 +1,34 @@
+#include "common.h"
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/sj_uni", sjuni_uuid);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/sj_uni", D_0055E6A0);
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/sj_uni", SJUNI_Error);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/sj_uni", SJUNI_Init);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/sj_uni", SJUNI_Finish);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/sj_uni", SJUNI_Create);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/sj_uni", SJUNI_Destroy);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/sj_uni", SJUNI_GetUuid);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/sj_uni", SJUNI_EntryErrFunc);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/sj_uni", SJUNI_Reset);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/sj_uni", SJUNI_GetNumData);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/sj_uni", SJUNI_GetChunk);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/sj_uni", SJUNI_PutChunk);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/sj_uni", SJUNI_UngetChunk);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/sj_uni", SJUNI_IsGetChunk);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/sj_uni", SJUNI_GetNumChunk);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/sj_uni", SJUNI_GetNumChainPool);
