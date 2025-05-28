880be06eee4e223bf6b2c249b9cb90332247f5c7
Artem Pstygo <apstygo.dev@gmail.com>
2025-05-28 23:43:50 +0300

    Add stubs

--- include/sf33rd/Source/PS2/mc/knjsub.h
+++ include/sf33rd/Source/PS2/mc/knjsub.h
@@ -3,16 +3,16 @@
 
 #include "types.h"
 
-void KnjFlush();
 void KnjInit(u32 type, u32 adrs, u32 disp_max, u32 top_dbp);
+void KnjFinish();
+void KnjSetSize(s32 dispw, s32 disph);
 void KnjLocate(s32 x, s32 y);
-void KnjPuts(const s8 *str);
-void KnjSetRgb(u32 color);
 void KnjSetColor(u32 color);
 void KnjSetAlpha(u32 alpha);
-void KnjSetSize(s32 dispw, s32 disph);
+void KnjSetRgb(u32 color);
+void KnjPuts(const s8 *str);
 void KnjPrintf(const s8 *fmt, ...);
+void KnjFlush();
 s32 KnjStrLen(const s8 *str);
-void KnjFinish();
 
 #endif
