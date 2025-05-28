87b5130db96a44ea467bb48b473efa1f06845ac8
Artem Pstygo <apstygo.dev@gmail.com>
2025-05-28 23:27:11 +0300

    Decompile savesub.c
    Co-authored-by: Mc-muffin <ederm14@hotmail.com>

--- include/sf33rd/Source/PS2/mc/knjsub.h
+++ include/sf33rd/Source/PS2/mc/knjsub.h
@@ -1,6 +1,18 @@
 #ifndef KNJSUB_H
 #define KNJSUB_H
 
+#include "types.h"
+
 void KnjFlush();
+void KnjInit(u32 type, u32 adrs, u32 disp_max, u32 top_dbp);
+void KnjLocate(s32 x, s32 y);
+void KnjPuts(const s8 *str);
+void KnjSetRgb(u32 color);
+void KnjSetColor(u32 color);
+void KnjSetAlpha(u32 alpha);
+void KnjSetSize(s32 dispw, s32 disph);
+void KnjPrintf(const s8 *fmt, ...);
+s32 KnjStrLen(const s8 *str);
+void KnjFinish();
 
 #endif
