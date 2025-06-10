29e013b765351b08b21bb155822645eea7bda5c0
Xkower8181 <xkowerlolz@gmail.com>
2025-06-11 00:59:16 +0200

    Fix function definition in cse.h

--- include/sf33rd/AcrSDK/MiddleWare/PS2/CapSndEng/cse.h
+++ include/sf33rd/AcrSDK/MiddleWare/PS2/CapSndEng/cse.h
@@ -25,8 +25,7 @@ typedef struct {
 
 s32 cseInitSndDrv();
 s32 cseExecServer();
-s32 cseTsbRequest(u16 bank, u16 code, s32 NumArgSets, s32 /* unused */, s32 /* unused */, s32 /* unused */,
-                  s32 /* unused */);
+s32 cseTsbRequest(u16 bank, u16 code, s32 NumArgSets, ...);
 s32 cseCheckVTransStatus(u32 vtrans_check_type);
 
 // Defined in color3rd.c
