94ea41cb75854d1f2b14102a7047a9a924465430
foolycooly95 <jackwoodman95@gmail.com>
2025-06-11 08:56:41 +0100

    Pull request suggestions

--- include/sf33rd/Source/Game/BBBSCOM.h
+++ include/sf33rd/Source/Game/BBBSCOM.h
@@ -1,18 +1,18 @@
 #ifndef BBBSCOM_H
 #define BBBSCOM_H
 
-#include "types.h"
 #include "structs.h"
+#include "types.h"
 
-typedef struct /* @anon24 */ {
+typedef struct {
     // total size: 0x26
-    signed short timer; // offset 0x0, size 0x2
-    signed short jmplv; // offset 0x2, size 0x2
-    signed short kosuu; // offset 0x4, size 0x2
-    signed short bbdat[4][4]; // offset 0x6, size 0x20
+    s16 timer;       // offset 0x0, size 0x2
+    s16 jmplv;       // offset 0x2, size 0x2
+    s16 kosuu;       // offset 0x4, size 0x2
+    s16 bbdat[4][4]; // offset 0x6, size 0x20
 } BBBSTable;
 
-extern const s32 bbbs_jump_level[4][2]; // size: 0x20, address: 0x4E58A0
+extern const s32 bbbs_jump_level[4][2];   // size: 0x20, address: 0x4E58A0
 extern const BBBSTable bbbs_level_00[18]; // size: 0x2AC, address: 0x4E58C0
 extern const BBBSTable bbbs_level_01[16]; // size: 0x260, address: 0x4E5B70
 extern const BBBSTable bbbs_level_02[15]; // size: 0x23A, address: 0x4E5DD0
@@ -24,9 +24,7 @@ extern const BBBSTable bbbs_level_07[17]; // size: 0x286, address: 0x4E68C0
 extern const BBBSTable bbbs_level_08[11]; // size: 0x1A2, address: 0x4E6B50
 extern const BBBSTable bbbs_level_09[12]; // size: 0x1C8, address: 0x4E6D00
 
-extern const BBBSTable * bbbs_table[2][5]; // size: 0x28, address: 0x5724A0
-
-// u8 bbbs_type; // size: 0x1, address: 0x57A2D4
+extern const BBBSTable *bbbs_table[2][5]; // size: 0x28, address: 0x5724A0
 
 void bbbs_com_execute(PLW *wk);
 void bbbs_com_initialize();
