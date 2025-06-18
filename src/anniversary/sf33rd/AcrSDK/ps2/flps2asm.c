152e9770373ac7e6ae6af05f55778181828ef103
WuGambinos <69660261+WuGambinos@users.noreply.github.com>
2025-06-18 18:23:06 -0400

    Decompile flps2asm.c

---
+++ src/anniversary/sf33rd/AcrSDK/ps2/flps2asm.c
@@ -0,0 +1,25 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/AcrSDK/ps2/flps2asm", flPS2_Mem_move);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/AcrSDK/ps2/flps2asm", flPS2_Mem_move16);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/AcrSDK/ps2/flps2asm", flPS2_Mem_move16_8A);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/AcrSDK/ps2/flps2asm", flPS2_Mem_move16_16A);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/AcrSDK/ps2/flps2asm", flPS2_Mem_move32);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/AcrSDK/ps2/flps2asm", flPS2_Mem_move32_8A);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/AcrSDK/ps2/flps2asm", flPS2_Mem_move32_16A);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/AcrSDK/ps2/flps2asm", flPS2_Mem_move64);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/AcrSDK/ps2/flps2asm", flPS2_Clear_Mem);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/AcrSDK/ps2/flps2asm", flPS2_Clear_Mem16_16A);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/AcrSDK/ps2/flps2asm", flPS2_Fill_Mem);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/AcrSDK/ps2/flps2asm", flPS2_Fill_Mem16_16A);
