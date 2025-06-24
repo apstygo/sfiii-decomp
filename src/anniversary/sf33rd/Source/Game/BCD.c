78350cfe99881e58224978a7248f1d263dddd6bc
foolycooly95 <jackwoodman95@gmail.com>
2025-06-24 22:30:04 +0100

    BCD

--- src/anniversary/sf33rd/Source/Game/BCD.c
+++ src/anniversary/sf33rd/Source/Game/BCD.c
@@ -1,3 +1,24 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/BCD", sbcd);
+s16 bcdext;
+
+u8 sbcd(u8 a, u8 b) {
+    s16 c, d;
+
+    if ((d = (b & 0xf) - (a & 0xf) - (bcdext & 1)) < 0) {
+        d += 10;
+        d |= 16;
+    }
+
+    c = (b & 0xf0) - (a & 0xf0) - (d & 0xf0);
+    d &= 0xF;
+
+    if ((d |= c) < 0) {
+        d += 160;
+        bcdext = 1;
+    } else {
+        bcdext = 0;
+    }
+
+    return d;
+}
