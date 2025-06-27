5ddfb0e52412e8f1c1a35dcb306793e1496276ad
Xkower8181 <xkowerlolz@gmail.com>
2025-06-27 16:34:41 +0200

    Decompile RANKING.c

--- include/sf33rd/Source/Game/bg_sub.h
+++ include/sf33rd/Source/Game/bg_sub.h
@@ -5,8 +5,10 @@
 #include "types.h"
 
 void suzi_sync_pos_set(WORK_Other *ewk);
+void Bg_Family_Set();
 void Bg_Family_Set_appoint(s32 num_of_bg);
 void bg_pos_hosei_sub2(s16 bg_no);
+void bg_pos_hosei2();
 void bg_work_clear();
 void compel_bg_init_position();
 void bg_etc_write(s16 type);
