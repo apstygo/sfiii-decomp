241dbb4d751adfc243b36214c723d464698b7ead
foolycooly95 <jackwoodman95@gmail.com>
2025-07-02 22:33:13 +0100

    LOSE_PL

---
+++ include/sf33rd/Source/Game/EFFC1.h
@@ -0,0 +1,8 @@
+#ifndef EFFC1_H
+#define EFFC1_H
+
+#include "types.h"
+
+s32 effect_C1_init(WORK *wk);
+
+#endif
---
+++ src/anniversary/sf33rd/Source/Game/EFFC1.c
@@ -0,0 +1,11 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFFC1", effect_C1_move);
+
+#if defined(TARGET_PS2)
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFFC1", effect_C1_init);
+#else
+s32 effect_C1_init(WORK *wk) {
+    not_implemented(__func__);
+}
+#endif
