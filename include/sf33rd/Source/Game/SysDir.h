7715656c5bfb174216b0c9796dfc6452d2770acc
foolycooly95 <jackwoodman95@gmail.com>
2025-08-04 16:48:19 +0100

    made a start

--- include/sf33rd/Source/Game/SysDir.h
+++ include/sf33rd/Source/Game/SysDir.h
@@ -12,6 +12,7 @@ extern s16 omop_round_timer;
 extern s16 omop_vt_bar_disp[2];
 extern s16 omop_st_bar_disp[2];
 extern s16 omop_b_block_ix[2];
+extern s16 omop_use_ex_gauge_ix[2];
 
 extern const s16 blok_b_omake[4];
 
