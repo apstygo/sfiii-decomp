b6e65857740ac8666228318bb1632334d5d722ff
foolycooly95 <jackwoodman95@gmail.com>
2025-08-06 13:46:11 +0100

    done

--- include/sf33rd/Source/Game/Sel_Data.h
+++ include/sf33rd/Source/Game/Sel_Data.h
@@ -3,6 +3,9 @@
 
 #include "types.h"
 
+extern const s8 Face_Cursor_Data[3][8];
 extern const u8 PL_Color_Data[20]; // size: 0x14, address: 0x552050
+extern const u8 Voice_Random_Data[2][4][3];
+extern const u8 Random_Stage_Data[2][32];
 
 #endif
