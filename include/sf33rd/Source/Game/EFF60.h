0876806d943c38969f89ef21c60bbf1b867620c0
foolycooly95 <jackwoodman95@gmail.com>
2025-08-18 21:55:02 +0100

    effect_60_init

--- include/sf33rd/Source/Game/EFF60.h
+++ include/sf33rd/Source/Game/EFF60.h
@@ -8,6 +8,7 @@ extern const s16 flash_0001[10];
 extern const s16 flash_0002[10];
 extern const s16 *flash_obj_data61[3];
 
+void effect_60_move(WORK_Other *ewk);
 s32 effect_60_init(s16 type);
 
 #endif
