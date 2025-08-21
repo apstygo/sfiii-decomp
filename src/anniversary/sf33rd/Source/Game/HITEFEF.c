e15236afb3dadaf472963c0c9771e13614e15195
foolycooly95 <jackwoodman95@gmail.com>
2025-08-21 03:43:38 +0100

    these should be WORK_Other because it's effect vs effect

--- src/anniversary/sf33rd/Source/Game/HITEFEF.c
+++ src/anniversary/sf33rd/Source/Game/HITEFEF.c
@@ -10,39 +10,39 @@ void effect_at_vs_effect_dm(s16 ix2, s16 ix) {
     s32 effect_02_init(WORK * wk, s32 dmgp, s32 mkst, s32 dmrl);
 #endif
 
-    WORK *as = q_hit_push[ix2];
-    WORK *ds = q_hit_push[ix];
+    WORK_Other *as = (WORK_Other *)q_hit_push[ix2];
+    WORK_Other *ds = (WORK_Other *)q_hit_push[ix];
 
-    ds->dm_rl = as->rl_flag;
-    as->dm_rl = ds->rl_flag;
-    cal_hit_mark_pos(as, ds, ix2, ix);
+    ds->wu.dm_rl = as->wu.rl_flag;
+    as->wu.dm_rl = ds->wu.rl_flag;
+    cal_hit_mark_pos(&as->wu, &ds->wu, ix2, ix);
 
-    if (ds->att.dipsw & 2) {
-        if (as->att.dipsw & 2) {
-            ds->dm_vital = 128;
-            as->dm_vital = 128;
+    if (ds->wu.att.dipsw & 2) {
+        if (as->wu.att.dipsw & 2) {
+            ds->wu.dm_vital = 128;
+            as->wu.dm_vital = 128;
         } else {
-            ds->dm_vital = 128;
-            as->dm_vital = 128;
+            ds->wu.dm_vital = 128;
+            as->wu.dm_vital = 128;
 
-            if (as->shell_vs_refrect == 0) {
-                ((WORK_Other *)as)->dm_refrect = 1;
-                ((WORK_Other *)as)->refrected = 1;
-                as->att_hit_ok = 1;
+            if (as->wu.shell_vs_refrect == 0) {
+                as->dm_refrect = 1;
+                as->refrected = 1;
+                as->wu.att_hit_ok = 1;
             }
         }
-    } else if (ds->id == 13) {
-        switch (as->work_id) {
+    } else if (ds->wu.id == 13) {
+        switch (as->wu.work_id) {
         case 4:
-            ds->dm_vital = as->vital_new;
-            as->dm_vital = ds->vital_new;
+            ds->wu.dm_vital = as->wu.vital_new;
+            as->wu.dm_vital = ds->wu.vital_new;
 
-            if (ds->dm_vital > ds->vital_new) {
-                ds->dm_vital = ds->vital_new;
+            if (ds->wu.dm_vital > ds->wu.vital_new) {
+                ds->wu.dm_vital = ds->wu.vital_new;
             }
 
-            if (as->dm_vital > as->vital_new) {
-                as->dm_vital = as->vital_new;
+            if (as->wu.dm_vital > as->wu.vital_new) {
+                as->wu.dm_vital = as->wu.vital_new;
             }
 
             break;
@@ -50,28 +50,28 @@ void effect_at_vs_effect_dm(s16 ix2, s16 ix) {
         default:
             break;
         }
-    } else if (ds->id == 122 || ds->id == 123) {
+    } else if (ds->wu.id == 122 || ds->wu.id == 123) {
         cal_damage_vitality((PLW *)as, (PLW *)ds);
-        as->dm_vital = 256;
+        as->wu.dm_vital = 256;
     } else {
-        ds->dm_vital = 256;
+        ds->wu.dm_vital = 256;
     }
 
-    if (ds->xyz[1].disp.pos > 0) {
-        as->hf.hit.effect = 2;
+    if (ds->wu.xyz[1].disp.pos > 0) {
+        as->wu.hf.hit.effect = 2;
     } else {
-        as->hf.hit.effect = 1;
+        as->wu.hf.hit.effect = 1;
     }
 
-    ds->routine_no[1] = 1;
-    ds->routine_no[2] = 0;
-    as->hit_stop = ds->hit_stop = 6;
-    ds->dm_dir = as->dir_atthit;
-    ds->dm_kind_of_waza = as->kind_of_waza;
+    ds->wu.routine_no[1] = 1;
+    ds->wu.routine_no[2] = 0;
+    as->wu.hit_stop = ds->wu.hit_stop = 6;
+    ds->wu.dm_dir = as->wu.dir_atthit;
+    ds->wu.dm_kind_of_waza = as->wu.kind_of_waza;
 
-    if (ds->id == 122 || ds->id == 123) {
-        effect_02_init(as, 2, 1, ds->dm_rl);
+    if (ds->wu.id == 122 || ds->wu.id == 123) {
+        effect_02_init(&as->wu, 2, 1, ds->wu.dm_rl);
     }
 
-    hit_pattern_extdat_check(as);
+    hit_pattern_extdat_check(&as->wu);
 }
