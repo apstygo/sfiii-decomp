1fadadd67417edb1c2610755b559d33eff128899
foolycooly95 <jackwoodman95@gmail.com>
2025-08-24 09:03:02 +0100

    removed unnecessary declares from .h files which only appear in dwarf once

--- include/sf33rd/Source/Game/EFF53.h
+++ include/sf33rd/Source/Game/EFF53.h
@@ -4,8 +4,6 @@
 #include "structs.h"
 #include "types.h"
 
-extern const s16 eff53_vanish_time[8];
-
 void effect_53_move(WORK_Other *ewk);
 s32 effect_53_init();
 
--- include/sf33rd/Source/Game/EFF68.h
+++ include/sf33rd/Source/Game/EFF68.h
@@ -4,8 +4,6 @@
 #include "structs.h"
 #include "types.h"
 
-extern const s16 eff68_data_tbl[52];
-
 void effect_68_move(WORK_Other *ewk);
 s32 effect_68_init();
 
