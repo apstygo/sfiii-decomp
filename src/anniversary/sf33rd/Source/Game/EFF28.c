9c16e91b86540864f562ca735c5802942cdafda7
foolycooly95 <jackwoodman95@gmail.com>
2025-08-24 09:16:49 +0100

    remove comment from EFF28

--- src/anniversary/sf33rd/Source/Game/EFF28.c
+++ src/anniversary/sf33rd/Source/Game/EFF28.c
@@ -1,6 +1,4 @@
 #include "sf33rd/Source/Game/EFF28.h"
 #include "common.h"
 
-// INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFF28", effect_28_move);
-
 void effect_28_move(WORK_Other * /* unused */) {}
