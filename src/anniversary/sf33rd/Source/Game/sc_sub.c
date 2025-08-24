321ed6cbbefb4631057ce3da2c4c56ac99f8d623
foolycooly95 <jackwoodman95@gmail.com>
2025-08-24 08:27:43 +0100

    EFFA0, EffA2, EFFA3

--- src/anniversary/sf33rd/Source/Game/sc_sub.c
+++ src/anniversary/sf33rd/Source/Game/sc_sub.c
@@ -494,11 +494,29 @@ void player_face() {
 
 INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/sc_sub", face_base_put);
 
+#if defined(TARGET_PS2)
 INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/sc_sub", hnc_set);
+#else
+void hnc_set(u8 num, u8 atr) {
+    not_implemented(__func__);
+}
+#endif
 
+#if defined(TARGET_PS2)
 INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/sc_sub", hnc_wipeinit);
+#else
+void hnc_wipeinit(u8 atr) {
+    not_implemented(__func__);
+}
+#endif
 
+#if defined(TARGET_PS2)
 INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/sc_sub", hnc_wipeout);
+#else
+s32 hnc_wipeout(u8 atr) {
+    not_implemented(__func__);
+}
+#endif
 
 INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/sc_sub", ci_set);
 
