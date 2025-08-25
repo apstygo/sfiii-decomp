ca29437e5ac50ab2b860059d217118a4496fa232
foolycooly95 <jackwoodman95@gmail.com>
2025-08-26 00:21:29 +0100

    EFF09.c

---
+++ src/anniversary/sf33rd/Source/Game/EFFB4.c
@@ -0,0 +1,7 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFFB4", effect_B4_move);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFFB4", effect_B4_init);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFFB4", s_mark_tbl);
