be152725e821fb163b4fd07e06f44c142398c9f3
foolycooly95 <jackwoodman95@gmail.com>
2025-08-28 18:37:31 +0100

    made a start

---
+++ src/anniversary/sf33rd/Source/Game/EFF96.c
@@ -0,0 +1,5 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFF96", effect_96_move);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFF96", effect_96_init);
---
+++ src/anniversary/sf33rd/Source/Game/EFFI9.c
@@ -0,0 +1,9 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFFI9", effect_I9_move);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFFI9", push_image_buff);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFFI9", init_image_buff);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFFI9", effect_I9_init);
