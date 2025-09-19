7014ac52c2046ff4cdbaae2c1cd890de90712b11
foolycooly95 <jackwoodman95@gmail.com>
2025-09-19 20:04:51 +0100

    needed a forward declare for urian(sic)_guard_se_check

--- src/anniversary/sf33rd/Source/Game/EFF02.c
+++ src/anniversary/sf33rd/Source/Game/EFF02.c
@@ -11,6 +11,8 @@
 #include "sf33rd/Source/Game/char_table.h"
 #include "sf33rd/Source/Game/workuser.h"
 
+void urian_guard_se_check(WORK_Other *ewk, PLW *twk, u16 oto);
+
 const s16 hit_mark_dir_table[16] = { 0, -1, -2, -3, -4, -5, -6, -7, 8, 7, 6, 5, 4, 3, 2, 1 };
 
 const HMDT hmdt[146] = { { 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0 },         { 266, 1, 1, 5, 9, 6, 0, 0, 0, 0, 0 },
