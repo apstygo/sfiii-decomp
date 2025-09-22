d6d3bb27af3ec86a40b35d636374e500920294d3
foolycooly95 <jackwoodman95@gmail.com>
2025-09-22 14:57:17 +0100

    added effect_dummy_init and effect_dummy_move. added effect_dummy_move to effmovejptbl and changed the comments so it's more obvious what's missing

--- src/anniversary/sf33rd/Source/Game/EFFXX.c
+++ src/anniversary/sf33rd/Source/Game/EFFXX.c
@@ -151,244 +151,246 @@
 #include "sf33rd/Source/Game/efff6.h"
 #include "sf33rd/Source/Game/effl6.h"
 
+s32 effect_dummy_init() {
+    return -1;
+}
+
+void effect_dummy_move() {}
+
 #if defined(TARGET_PS2)
 INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFFXX", effmovejptbl);
 #else
 const void (*effmovejptbl[229])() = {
-    effect_00_move, // effect_00_move
-    effect_01_move, // effect_01_move
-    effect_02_move, // effect_02_move
-    effect_03_move, // effect_03_move
-    effect_04_move, // effect_04_move
-    effect_05_move, // effect_05_move
-    effect_06_move, // effect_06_move
-    effect_07_move, // effect_07_move
-    effect_08_move, // effect_08_move
-    effect_09_move, // effect_09_move
-    effect_10_move, // effect_10_move
-    effect_11_move, // effect_11_move
-    effect_12_move, // effect_12_move
-    effect_13_move, // effect_13_move
-    effect_14_move, // effect_14_move
-    effect_15_move, // effect_15_move
-    effect_16_move, // effect_16_move
-    effect_17_move, // effect_17_move
-    effect_18_move, // effect_18_move
-    effect_19_move, // effect_19_move
-    effect_20_move, // effect_20_move
-    effect_21_move, // effect_21_move
-    effect_22_move, // effect_22_move
-    effect_23_move, // effect_23_move
-    effect_24_move, // effect_24_move
-    effect_25_move, // effect_25_move
-    effect_26_move, // effect_26_move
-    effect_27_move, // effect_27_move
-    effect_28_move, // effect_28_move
-    effect_29_move, // effect_29_move
-    effect_30_move, // effect_30_move
-    effect_31_move, // effect_31_move
-    effect_32_move, // effect_32_move
-    effect_33_move, // effect_33_move
-    effect_34_move, // effect_34_move
-    effect_35_move, // effect_35_move
-    effect_36_move, // effect_36_move
-    effect_37_move, // effect_37_move
-    effect_38_move, // effect_38_move
-    effect_39_move, // effect_39_move
-    effect_40_move, // effect_40_move
-    effect_41_move, // effect_41_move
-    effect_42_move, // effect_42_move
-    effect_43_move, // effect_43_move
-    effect_44_move, // effect_44_move
-    effect_45_move, // effect_45_move
-    effect_46_move, // effect_46_move
-    NULL,           // effect_47_move
-    effect_48_move, // effect_48_move
-    effect_49_move, // effect_49_move
-    effect_50_move, // effect_50_move
-    effect_51_move, // effect_51_move
-    effect_52_move, // effect_52_move
-    effect_53_move, // effect_53_move
-    effect_54_move, // effect_54_move
-    effect_55_move, // effect_55_move
-    effect_56_move, // effect_56_move
-    effect_57_move, // effect_57_move
-    effect_58_move, // effect_58_move
-    effect_59_move, // effect_59_move
-    effect_60_move, // effect_60_move
-    effect_61_move, // effect_61_move
-    effect_62_move, // effect_62_move
-    effect_63_move, // effect_63_move
-    effect_64_move, // effect_64_move
-    effect_65_move, // effect_65_move
-    effect_66_move, // effect_66_move
-    effect_67_move, // effect_67_move
-    effect_68_move, // effect_68_move
-    effect_69_move, // effect_69_move
-    effect_70_move, // effect_70_move
-    effect_71_move, // effect_71_move
-    effect_72_move, // effect_72_move
-    NULL,           // effect_73_move
-    NULL,           // effect_74_move
-    effect_75_move, // effect_75_move
-    effect_76_move, // effect_76_move
-    NULL,           // effect_77_move
-    effect_78_move, // effect_78_move
-    effect_79_move, // effect_79_move
-    effect_80_move, // effect_80_move
-    effect_81_move, // effect_81_move
-    effect_82_move, // effect_82_move
-    effect_83_move, // effect_83_move
-    effect_84_move, // effect_84_move
-    effect_85_move, // effect_85_move
-    effect_86_move, // effect_86_move
-    effect_87_move, // effect_87_move
-    NULL,           // effect_dummy_move
-    NULL,           // effect_dummy_move
-    NULL,           // effect_90_move
-    effect_91_move, // effect_91_move
-    effect_92_move, // effect_92_move
-    effect_93_move, // effect_93_move
-    effect_94_move, // effect_94_move
-    effect_95_move, // effect_95_move
-    effect_96_move, // effect_96_move
-    effect_97_move, // effect_97_move
-    effect_98_move, // effect_98_move
-    effect_99_move, // effect_99_move
-    effect_A0_move, // effect_A0_move
-    effect_A1_move, // effect_A1_move
-    effect_A2_move, // effect_A2_move
-    effect_A3_move, // effect_A3_move
-    NULL,           // effect_dummy_move
-    effect_A5_move, // effect_A5_move
-    effect_A6_move, // effect_A6_move
-    effect_A7_move, // effect_A7_move
-    effect_A8_move, // effect_A8_move
-    effect_A9_move, // effect_A9_move
-    effect_B0_move, // effect_B0_move
-    effect_B1_move, // effect_B1_move
-    effect_B2_move, // effect_B2_move
-    NULL,           // effect_B3_move
-    effect_B4_move, // effect_B4_move
-    NULL,           // effect_B5_move
-    NULL,           // effect_B6_move
-    effect_B7_move, // effect_B7_move
-    NULL,           // effect_B8_move
-    NULL,           // effect_B9_move
-    NULL,           // effect_C0_move
-    effect_C1_move, // effect_C1_move
-    effect_C2_move, // effect_C2_move
-    NULL,           // effect_C3_move
-    effect_C4_move, // effect_C4_move
-    NULL,           // effect_C5_move
-    NULL,           // effect_C6_move
-    NULL,           // effect_C7_move
-    effect_C8_move, // effect_C8_move
-    NULL,           // effect_C9_move
-    NULL,           // effect_D0_move
-    NULL,           // effect_D1_move
-    NULL,           // effect_dummy_move
-    NULL,           // effect_D3_move
-    NULL,           // effect_D4_move
-    NULL,           // effect_D5_move
-    NULL,           // effect_D6_move
-    NULL,           // effect_D7_move
-    effect_D8_move, // effect_D8_move
-    NULL,           // effect_D9_move
-    effect_E0_move, // effect_E0_move
-    effect_E1_move, // effect_E1_move
-    NULL,           // effect_E2_move
-    effect_E3_move, // effect_E3_move
-    effect_E4_move, // effect_E4_move
-    effect_E5_move, // effect_E5_move
-    effect_E6_move, // effect_E6_move
-    effect_E7_move, // effect_E7_move
-    effect_E8_move, // effect_E8_move
-    effect_E9_move, // effect_E9_move
-    NULL,           // effect_F0_move
-    NULL,           // effect_dummy_move
-    effect_F2_move, // effect_F2_move
-    NULL,           // effect_dummy_move
-    effect_F4_move, // effect_F4_move
-    effect_F5_move, // effect_F5_move
-    effect_F6_move, // effect_F6_move
-    NULL,           // effect_dummy_move
-    NULL,           // effect_F8_move
-    effect_F9_move, // effect_F9_move
-    effect_G0_move, // effect_G0_move
-    NULL,           // effect_dummy_move
-    NULL,           // effect_dummy_move
-    NULL,           // effect_G3_move
-    NULL,           // effect_G4_move
-    NULL,           // effect_G5_move
-    NULL,           // effect_G6_move
-    NULL,           // effect_G7_move
-    NULL,           // effect_G8_move
-    NULL,           // effect_G9_move
-    effect_H0_move, // effect_H0_move
-    effect_H1_move, // effect_H1_move
-    effect_H2_move, // effect_H2_move
-    NULL,           // effect_dummy_move
-    NULL,           // effect_dummy_move
-    NULL,           // effect_dummy_move
-    effect_H6_move, // effect_H6_move
-    NULL,           // effect_dummy_move
-    NULL,           // effect_dummy_move
-    effect_H9_move, // effect_H9_move
-    NULL,           // effect_I0_move
-    NULL,           // effect_dummy_move
-    NULL,           // effect_dummy_move
-    effect_I3_move, // effect_I3_move
-    effect_I4_move, // effect_I4_move
-    NULL,           // effect_dummy_move
-    NULL,           // effect_I6_move
-    effect_I7_move, // effect_I7_move
-    NULL,           // effect_I8_move
-    effect_I9_move, // effect_I9_move
-    effect_J0_move, // effect_J0_move
-    NULL,           // effect_dummy_move
-    effect_J2_move, // effect_J2_move
-    NULL,           // effect_dummy_move
-    effect_J4_move, // effect_J4_move
-    NULL,           // effect_dummy_move
-    effect_J6_move, // effect_J6_move
-    effect_J7_move, // effect_J7_move
-    effect_J8_move, // effect_J8_move
-    NULL,           // effect_J9_move
-    NULL,           // effect_dummy_move
-    NULL,           // effect_dummy_move
-    NULL,           // effect_K2_move
-    NULL,           // effect_K3_move
-    NULL,           // effect_K4_move
-    NULL,           // effect_K5_move
-    effect_K6_move, // effect_K6_move
-    NULL,           // effect_K7_move
-    NULL,           // effect_K8_move
-    NULL,           // effect_K9_move
-    effect_L0_move, // effect_L0_move
-    NULL,           // effect_L1_move
-    effect_L2_move, // effect_L2_move
-    effect_L3_move, // effect_L3_move
-    effect_L4_move, // effect_L4_move
-    NULL,           // effect_L5_move
-    effect_L6_move, // effect_L6_move
-    NULL,           // effect_L7_move
-    NULL,           // effect_L8_move
-    NULL,           // effect_L9_move
-    NULL,           // effect_M0_move
-    NULL,           // effect_M1_move
-    effect_M2_move, // effect_M2_move
-    NULL,           // effect_M3_move
-    NULL,           // effect_dummy_move
-    NULL,           // effect_M5_move
-    NULL,           // effect_M6_move
-    NULL,           // effect_M7_move
-    NULL,           // effect_M8_move
+    effect_00_move,
+    effect_01_move,
+    effect_02_move,
+    effect_03_move,
+    effect_04_move,
+    effect_05_move,
+    effect_06_move,
+    effect_07_move,
+    effect_08_move,
+    effect_09_move,
+    effect_10_move,
+    effect_11_move,
+    effect_12_move,
+    effect_13_move,
+    effect_14_move,
+    effect_15_move,
+    effect_16_move,
+    effect_17_move,
+    effect_18_move,
+    effect_19_move,
+    effect_20_move,
+    effect_21_move,
+    effect_22_move,
+    effect_23_move,
+    effect_24_move,
+    effect_25_move,
+    effect_26_move,
+    effect_27_move,
+    effect_28_move,
+    effect_29_move,
+    effect_30_move,
+    effect_31_move,
+    effect_32_move,
+    effect_33_move,
+    effect_34_move,
+    effect_35_move,
+    effect_36_move,
+    effect_37_move,
+    effect_38_move,
+    effect_39_move,
+    effect_40_move,
+    effect_41_move,
+    effect_42_move,
+    effect_43_move,
+    effect_44_move,
+    effect_45_move,
+    effect_46_move,
+    NULL, // effect_47_move
+    effect_48_move,
+    effect_49_move,
+    effect_50_move,
+    effect_51_move,
+    effect_52_move,
+    effect_53_move,
+    effect_54_move,
+    effect_55_move,
+    effect_56_move,
+    effect_57_move,
+    effect_58_move,
+    effect_59_move,
+    effect_60_move,
+    effect_61_move,
+    effect_62_move,
+    effect_63_move,
+    effect_64_move,
+    effect_65_move,
+    effect_66_move,
+    effect_67_move,
+    effect_68_move,
+    effect_69_move,
+    effect_70_move,
+    effect_71_move,
+    effect_72_move,
+    NULL, // effect_73_move
+    NULL, // effect_74_move
+    effect_75_move,
+    effect_76_move,
+    NULL, // effect_77_move
+    effect_78_move,
+    effect_79_move,
+    effect_80_move,
+    effect_81_move,
+    effect_82_move,
+    effect_83_move,
+    effect_84_move,
+    effect_85_move,
+    effect_86_move,
+    effect_87_move,
+    effect_dummy_move,
+    effect_dummy_move,
+    NULL, // effect_90_move
+    effect_91_move,
+    effect_92_move,
+    effect_93_move,
+    effect_94_move,
+    effect_95_move,
+    effect_96_move,
+    effect_97_move,
+    effect_98_move,
+    effect_99_move,
+    effect_A0_move,
+    effect_A1_move,
+    effect_A2_move,
+    effect_A3_move,
+    effect_dummy_move,
+    effect_A5_move,
+    effect_A6_move,
+    effect_A7_move,
+    effect_A8_move,
+    effect_A9_move,
+    effect_B0_move,
+    effect_B1_move,
+    effect_B2_move,
+    NULL, // effect_B3_move
+    effect_B4_move,
+    NULL, // effect_B5_move
+    NULL, // effect_B6_move
+    effect_B7_move,
+    NULL, // effect_B8_move
+    NULL, // effect_B9_move
+    NULL, // effect_C0_move
+    effect_C1_move,
+    effect_C2_move,
+    NULL, // effect_C3_move
+    effect_C4_move,
+    NULL, // effect_C5_move
+    NULL, // effect_C6_move
+    NULL, // effect_C7_move
+    effect_C8_move,
+    NULL, // effect_C9_move
+    NULL, // effect_D0_move
+    NULL, // effect_D1_move
+    effect_dummy_move,
+    NULL, // effect_D3_move
+    NULL, // effect_D4_move
+    NULL, // effect_D5_move
+    NULL, // effect_D6_move
+    NULL, // effect_D7_move
+    effect_D8_move,
+    NULL, // effect_D9_move
+    effect_E0_move,
+    effect_E1_move,
+    NULL, // effect_E2_move
+    effect_E3_move,
+    effect_E4_move,
+    effect_E5_move,
+    effect_E6_move,
+    effect_E7_move,
+    effect_E8_move,
+    effect_E9_move,
+    NULL, // effect_F0_move
+    effect_dummy_move,
+    effect_F2_move,
+    effect_dummy_move,
+    effect_F4_move,
+    effect_F5_move,
+    effect_F6_move,
+    effect_dummy_move,
+    NULL, // effect_F8_move
+    effect_F9_move,
+    effect_G0_move,
+    effect_dummy_move,
+    effect_dummy_move,
+    NULL, // effect_G3_move
+    NULL, // effect_G4_move
+    NULL, // effect_G5_move
+    NULL, // effect_G6_move
+    NULL, // effect_G7_move
+    NULL, // effect_G8_move
+    NULL, // effect_G9_move
+    effect_H0_move,
+    effect_H1_move,
+    effect_H2_move,
+    effect_dummy_move,
+    effect_dummy_move,
+    effect_dummy_move,
+    effect_H6_move,
+    effect_dummy_move,
+    effect_dummy_move,
+    effect_H9_move,
+    NULL, // effect_I0_move
+    effect_dummy_move,
+    effect_dummy_move,
+    effect_I3_move,
+    effect_I4_move,
+    effect_dummy_move,
+    NULL, // effect_I6_move
+    effect_I7_move,
+    NULL, // effect_I8_move
+    effect_I9_move,
+    effect_J0_move,
+    effect_dummy_move,
+    effect_J2_move,
+    effect_dummy_move,
+    effect_J4_move,
+    effect_dummy_move,
+    effect_J6_move,
+    effect_J7_move,
+    effect_J8_move,
+    NULL, // effect_J9_move
+    effect_dummy_move,
+    effect_dummy_move,
+    NULL, // effect_K2_move
+    NULL, // effect_K3_move
+    NULL, // effect_K4_move
+    NULL, // effect_K5_move
+    effect_K6_move,
+    NULL, // effect_K7_move
+    NULL, // effect_K8_move
+    NULL, // effect_K9_move
+    effect_L0_move,
+    NULL, // effect_L1_move
+    effect_L2_move,
+    effect_L3_move,
+    effect_L4_move,
+    NULL, // effect_L5_move
+    effect_L6_move,
+    NULL, // effect_L7_move
+    NULL, // effect_L8_move
+    NULL, // effect_L9_move
+    NULL, // effect_M0_move
+    NULL, // effect_M1_move
+    effect_M2_move,
+    NULL, // effect_M3_move
+    effect_dummy_move,
+    NULL, // effect_M5_move
+    NULL, // effect_M6_move
+    NULL, // effect_M7_move
+    NULL, // effect_M8_move
 };
 #endif
 
 INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFFXX", effinitjptbl);
-
-INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFFXX", effect_dummy_init);
-
-INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFFXX", effect_dummy_move);
