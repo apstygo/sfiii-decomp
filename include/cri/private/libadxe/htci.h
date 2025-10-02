09f48b7ae17f016a0a2002f104e739b9999fa3dd
foolycooly95 <jackwoodman95@gmail.com>
2025-10-02 17:16:08 +0100

    a few more cri functions

---
+++ include/cri/private/libadxe/htci.h
@@ -0,0 +1,6 @@
+#ifndef HTCI_H
+#define HTCI_H
+
+void htCiGetInterface();
+
+#endif
--- src/anniversary/cri/libadxe/adx_dcd5.c
+++ src/anniversary/cri/libadxe/adx_dcd5.c
@@ -14,12 +14,13 @@ INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/adx_dcd5", ADX_DecodeSte4A
 
 INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/adx_dcd5", ADX_DecodeSte4AsSte);
 
-#if defined(TARGET_PS2)
-INCLUDE_ASM("asm/anniversary/nonmatchings/cri/libadxe/adx_dcd5", ADX_DecodeSte4);
-#else
-Sint32 ADX_DecodeSte4(Sint32, Sint32, Sint32, Sint16*, Sint32, Sint16*, Sint16, Sint16, Sint16*, Sint16, Sint16) {
-    not_implemented(__func__);
+void ADX_DecodeSte4(int32_t arg0, int32_t arg1, int32_t arg2, int32_t arg3, int arg4, int arg5, short arg6, short arg7,
+                    int arg8, short arg9, short arg10) {
+    if (adx_decode_output_mono_flag == 0) {
+        ADX_DecodeSte4AsSte(arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10);
+    } else {
+        ADX_DecodeSte4AsMono(arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10);
+    }
 }
-#endif
 
 INCLUDE_RODATA("asm/anniversary/nonmatchings/cri/libadxe/adx_dcd5", AdxQtbl);
